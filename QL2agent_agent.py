df73f5221b7588b0431c381533f2a7d344062a74
Mikaela Funkquist <60342871+mikaelafunkquist97@users.noreply.github.com>
2020-02-13 10:47:05 +0100

    har tagit bort state listorna, behövs inte ens. isTerminalState går nu mycket fortare

--- QL2agent_agent.py
+++ QL2agent_agent.py
@@ -1,22 +1,25 @@
 class Agent:
     def __init__(self, pickup, dropoff, n, m):
-        # self.dropoff = dropoff
+        self.dropoff = dropoff
+        self.pickup = pickup
         self.n = n
         self.m = m
 
-        self.stateSpace = [i for i in range((n*m)**2) if i not in range(dropoff * n*m, dropoff * n*m + n*m)]
+        #self.stateSpace = [i for i in range((n*m)**2) if i not in range(dropoff * n*m, dropoff * n*m + n*m)]
         self.reward = 0
         self.posSpace = [i for i in range(n*m)]
 
         # self.stateSpace.remove([dropoff*100:dropoff*100+99])
 
-        self.stateSpacePlus = [i for i in range((n*m)**2)]
+        #self.stateSpacePlus = [i for i in range((n*m)**2)]
         self.possibleActions = ['Up', 'Down', 'Left', 'Right', 'Stay']
 
         self.Q = {}
-        for state in self.stateSpacePlus:
+        #for state in self.stateSpacePlus:
+        for i in range((n*m)**2):
             for action in self.possibleActions:
-                self.Q[state, action] = 0
+        #        self.Q[state, action] = 0
+                self.Q[i, action] = 0
 
         self.agentPosition = pickup
 
@@ -25,7 +28,11 @@ class Agent:
         self.agentPosition = pickup
 
     def isTerminalState(self, state):
-        return state in self.stateSpacePlus and state not in self.stateSpace
+        #return state in self.stateSpacePlus and state not in self.stateSpace
+        if self.agentPosition == self.dropoff:
+            return True
+        else:
+            return False
 
     def updatePosition(self, newPos):
         self.agentPosition = newPos
@@ -38,3 +45,4 @@ class Agent:
 
     def updateReward(self, reward):
         self.reward = reward
+
