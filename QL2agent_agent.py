e82c5ef04ac864973d2b9f57a9ea50e4b96d8f85
Mikaela Funkquist <60342871+mikaelafunkquist97@users.noreply.github.com>
2020-02-14 15:18:49 +0100

    Newest version

--- QL2agent_agent.py
+++ QL2agent_agent.py
@@ -5,20 +5,13 @@ class Agent:
         self.n = n
         self.m = m
 
-        #self.stateSpace = [i for i in range((n*m)**2) if i not in range(dropoff * n*m, dropoff * n*m + n*m)]
         self.reward = 0
         self.posSpace = [i for i in range(n*m)]
-
-        #self.stateSpace.remove([dropoff*100:dropoff*100+99])
-
-        #self.stateSpacePlus = [i for i in range((n*m)**2)]
         self.possibleActions = ['Up', 'Down', 'Left', 'Right', 'Stay']
 
         self.Q = {}
-        #for state in self.stateSpacePlus:
         for i in range((n*m)**2):
             for action in self.possibleActions:
-        #        self.Q[state, action] = 0
                 self.Q[i, action] = 0
 
         self.agentPosition = pickup
@@ -28,7 +21,6 @@ class Agent:
         self.agentPosition = pickup
 
     def isTerminalState(self, resultingPos):
-        #return state in self.stateSpacePlus and state not in self.stateSpace
         if resultingPos == self.dropoff:
             return True
         else:
@@ -39,9 +31,6 @@ class Agent:
 
     def getState(self, resultingPos, otherAgentPos):
         return resultingPos * self.n*self.m + otherAgentPos
-
-    def updateQmatrix(self, ):
-        pass
 
     def updateReward(self, reward):
         self.reward = reward
