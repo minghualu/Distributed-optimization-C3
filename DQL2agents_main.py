b8b295e9747a0aa83016c2602d1da0c20b058ae5
Mikaela Funkquist <60342871+mikaelafunkquist97@users.noreply.github.com>
2020-04-10 21:48:01 +0200

    Update DQL2agents_main.py

--- DQL2agents_main.py
+++ DQL2agents_main.py
@@ -1,60 +1,64 @@
-from DQL_new_env import *
+from DQL_3agents_env import *
 from DQL_agentclass import *
-# from testDQL import *
+from testDQL import *
 import matplotlib.pyplot as plt
-import pickle
 
 def main():
-    n = 5
-    m = 5
-    NumGames = 100
-    NumTimes = 100
+    n = 6
+    m = 6
+    NumGames = 3000
     epsilon = 1
     epsilon_min = 0.01
-    env = Warehouse(n, m, 0, (m-1)*n, n*m-1, m-1)   # Start in upper and lower left and ending in the other end of the diagonals
-    # env = Warehouse(n, m, 0, 12, 15, 3)
-    # env = Warehouse(n, m, 0, 20, 24, 4)
-    #state_size = 100 
-    state_size = n*m 
+    env = Warehouse(n, m, 0, n*(m-1), n-1, n*m-1, n-1, n*(m-1))
+    #state_size = n*m 
+    state_size = 3
     action_size = 4
-    agents = [DQNAgent(state_size, action_size), DQNAgent(state_size, action_size)]
-    #done = False
+    agents = [DQNAgent(state_size, action_size), DQNAgent(state_size, action_size), DQNAgent(state_size, action_size)]
     batch_size = 32
     totalReward = np.zeros(NumGames)
 
     for i in range(NumGames):
-        replays = [0, 0]
+        replays = [0, 0, 0]
         epRewards = 0
         env.reset()
         # print()
         # print("Game number: {}, Initial state: {}".format(i+1, env.state), end = '')
-        done = [False, False]
-        for time in range(NumTimes):
+        done = [False, False, False]
+        info = [False, False, False]
+        for time in range(100):
             #print(time)
-            for j in range(2):
-                if done[j]:
+            for j in range(3):
+                if done[j] or info[j]:
                     continue
-                #env.render()
-                curr_state = env.state.copy()
+
+                curr_state = [env.agentsPos[0], env.agentsPos[1], env.agentsPos[2]]
+                curr_state = np.reshape(curr_state, [1, state_size])
                 action = agents[j].act(curr_state, epsilon)
-                next_state, reward, done[j], info = env.step(action, j)
-                # print("Agent: {}, action: {}, curr_state: {}, next_state: {}, reward: {}, done: {}, info: {}"
+                next_state, reward, done[j], info[j], otherAgentNr = env.step(action, j)
+                next_state = [env.agentsPos[0], env.agentsPos[1], env.agentsPos[2]]
+                next_state = np.reshape(next_state, [1, state_size])
+                
+                #print("Agent: {}, action: {}, \t curr_state: {}, \t next_state: {}, \t reward: {}, \t done: {}, \t info: {}"
                 #       .format(j+1, action, curr_state, next_state, reward, done[j], info))
                 #reward = reward if not done else -10
                 #next_state = np.reshape(next_state, [1, state_size])
+                
                 agents[j].memorize(curr_state, action, reward, next_state, done[j])
                 epRewards += reward
-
-                # If the agents have collided
-                if info == True:
-                    break
                 
+                if otherAgentNr != None:
+                    info[int(otherAgentNr)-1] = True
+
                 if len(agents[j].memory) > batch_size:
                     # print('Replay agent #{}'.format(j+1))
                     replays[j] += 1
                     agents[j].replay(batch_size)
+
+                # If the agents have collided
+                if info[j]:
+                    continue
             
-            if (done[0] and done[1]) or info == True:
+            if (done[0] and done[1] and done[2]) or (info[0] and info[1] and info[2]):
                 break
 
         if epsilon > epsilon_min:
@@ -66,15 +70,10 @@ def main():
         print("Game: {}/{}, \t epRewards: {:3}, \t done: {}, \t collided: {}, \t time: {:3}, \t replays: {},   \t epsilon: {:.2}"
               .format(i+1, NumGames, epRewards, done, info, time, replays, epsilon))
 
-    with open('env.pkl', 'wb') as output:  # Overwrites any existing file.
-        pickle.dump(env, output, pickle.HIGHEST_PROTOCOL)
-    with open('agents.pkl', 'wb') as output:  # Overwrites any existing file.
-        pickle.dump(agents, output, pickle.HIGHEST_PROTOCOL)
-
     plt.plot(totalReward)
     plt.show()
     
-    # testDQL2(agents, env)
+    #testDQL2(agents, env)
 
 
 main()
