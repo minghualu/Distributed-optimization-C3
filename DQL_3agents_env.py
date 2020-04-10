cedbc6744b3b4357f89f5706c2b69c986c2154c6
Mikaela Funkquist <60342871+mikaelafunkquist97@users.noreply.github.com>
2020-04-10 21:52:50 +0200

    Update DQL_3agents_env.py

--- DQL_3agents_env.py
+++ DQL_3agents_env.py
@@ -1,79 +1,111 @@
-from DQL_3agents_env import *
-from DQL_agentclass import *
-from testDQL import *
-import matplotlib.pyplot as plt
+import numpy as np
 
-def main():
-    n = 6
-    m = 6
-    NumGames = 3000
-    epsilon = 1
-    epsilon_min = 0.01
-    env = Warehouse(n, m, 0, n*(m-1), n-1, n*m-1, n-1, n*(m-1))
-    #state_size = n*m 
-    state_size = 3
-    action_size = 4
-    agents = [DQNAgent(state_size, action_size), DQNAgent(state_size, action_size), DQNAgent(state_size, action_size)]
-    batch_size = 32
-    totalReward = np.zeros(NumGames)
+class Warehouse:
+    def __init__(self, n, m, start1, start2, start3, goal1, goal2, goal3):
+        self.n = n
+        self.m = m
+        self.start = [start1, start2, start3]
+        self.goal = [goal1, goal2, goal3]
+        self.grid = np.zeros((n, m))
+        self.state = np.zeros((1, n*m))
+        self.state[0][start1] = 1
+        self.state[0][start2] = 2
+        self.state[0][start3] = 3
+        self.agentsPos = [start1, start2, start3]
 
-    for i in range(NumGames):
-        replays = [0, 0, 0]
-        epRewards = 0
-        env.reset()
-        # print()
-        # print("Game number: {}, Initial state: {}".format(i+1, env.state), end = '')
-        done = [False, False, False]
-        info = [False, False, False]
-        for time in range(100):
-            #print(time)
-            for j in range(3):
-                if done[j] or info[j]:
-                    continue
+        self.posSpace = [i for i in range(n*m)]
 
-                curr_state = [env.agentsPos[0], env.agentsPos[1], env.agentsPos[2]]
-                curr_state = np.reshape(curr_state, [1, state_size])
-                action = agents[j].act(curr_state, epsilon)
-                next_state, reward, done[j], info[j], otherAgentNr = env.step(action, j)
-                next_state = [env.agentsPos[0], env.agentsPos[1], env.agentsPos[2]]
-                next_state = np.reshape(next_state, [1, state_size])
-                
-                #print("Agent: {}, action: {}, \t curr_state: {}, \t next_state: {}, \t reward: {}, \t done: {}, \t info: {}"
-                #       .format(j+1, action, curr_state, next_state, reward, done[j], info))
-                #reward = reward if not done else -10
-                #next_state = np.reshape(next_state, [1, state_size])
-                
-                agents[j].memorize(curr_state, action, reward, next_state, done[j])
-                epRewards += reward
-                
-                if otherAgentNr != None:
-                    info[int(otherAgentNr)-1] = True
+        # Defining all actions
+        self.actionSpace = [-self.m, self.m, -1, 1]
+        self.possibleActions = [0, 1, 2, 3] #Up, down, left, right
 
-                if len(agents[j].memory) > batch_size:
-                    # print('Replay agent #{}'.format(j+1))
-                    replays[j] += 1
-                    agents[j].replay(batch_size)
+        # Walls
+        #self.walls = [1, 11, 21, 31, 41, 51, 61, 93, 83, 73, 63, 53]
+        self.walls = []
 
-                # If the agents have collided
-                if info[j]:
-                    continue
-            
-            if (done[0] and done[1] and done[2]) or (info[0] and info[1] and info[2]):
-                break
+    # Translate to x and y coordinates
+    def getAgentRowAndColumn(self, agent_nr):
+        x = self.agentsPos[agent_nr] // self.m
+        y = self.agentsPos[agent_nr] % self.m
+        return x, y
 
-        if epsilon > epsilon_min:
-            epsilon -= 1/NumGames
-            if epsilon < epsilon_min:
-                epsilon = epsilon_min
 
-        totalReward[i] = epRewards
-        print("Game: {}/{}, \t epRewards: {:3}, \t done: {}, \t collided: {}, \t time: {:3}, \t replays: {},   \t epsilon: {:.2}"
-              .format(i+1, NumGames, epRewards, done, info, time, replays, epsilon))
+    def getWalls(self, wall):
+        x = wall // self.m
+        y = wall % self.m
+        if self.grid[x][y] != 1:
+            self.grid[x][y] = 2
+        return x, y
 
-    plt.plot(totalReward)
-    plt.show()
+    # Prohibits wrong movement
+    def offGridMove(self, newPos, oldPos):
+        # if we move into a row not in the grid
+        if newPos in self.walls:
+            return True
+        if newPos not in self.posSpace:
+            return True
+        # if we're trying to wrap around to next row
+        elif oldPos % self.m == 0 and newPos % self.m == self.m - 1:
+            return True
+        elif oldPos % self.m == self.m - 1 and newPos % self.m == 0:
+            return True
+        else:
+            return False
     
-    #testDQL2(agents, env)
+    def isTerminalState(self, resultingPos, agent_nr):
+        #if self.state[self.goal[agent_nr]] == 1:
+        #    return True
+        #else:
+        #    return False
+        if resultingPos == self.goal[agent_nr]:
+            return True
+        else:
+            return False
+        
+    def updateState(self, currentPos, resultingPos):
+        agent = self.state[0][currentPos]
+        self.state[0][currentPos] = 0
+        self.state[0][resultingPos] = agent
 
+    # Defining one step, with rewards
+    def step(self, action, agent_nr):
+        #x, y = self.getAgentRowAndColumn(agent)
 
-main()
+        currentPos = self.agentsPos[agent_nr]
+        resultingPos = currentPos + self.actionSpace[action]
+        
+        if not self.offGridMove(resultingPos, currentPos):
+            if self.state[0][resultingPos] == 0:
+                if self.isTerminalState(resultingPos, agent_nr):
+                    reward = 50 
+                    # print('Agent #{} reached goal'.format(agent_nr+1))
+                else:
+                    reward = -1
+            else:
+                #Collided with another agent
+                reward = -100
+                otherAgentNr = self.state[0][resultingPos]
+                # print('collided')
+                #self.updateState(currentPos, resultingPos)
+                return self.state, reward, False, True, otherAgentNr
+
+            #self.setGrid(resultingPos, agent)
+            self.updateState(currentPos, resultingPos)
+            self.agentsPos[agent_nr] = resultingPos
+            return self.state, reward, self.isTerminalState(resultingPos, agent_nr), False, None
+        else:
+            #Collided with wall
+            reward = -5
+            return self.state, reward, self.isTerminalState(currentPos, agent_nr), False, None
+
+    def reset(self):
+        #self.grid = np.zeros((self.n, self.m))
+        self.state = np.zeros((1, self.n*self.m))
+        self.state[0][self.start[0]] = 1
+        self.state[0][self.start[1]] = 2
+        self.state[0][self.start[2]] = 3
+        self.agentsPos = [self.start[0], self.start[1], self.start[2]]
+
+    # Random action while exploring
+    def actionSpaceSample(self):
+        return np.random.choice(self.possibleActions)
